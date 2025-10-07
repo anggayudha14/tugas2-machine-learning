6cd7f1c676d0a194fdc9a029ff467f21c0417043
anggayudha14 <164121561+anggayudha14@users.noreply.github.com>
2025-10-07 16:14:18 +0700

    Update to use Waitress for production

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 # Activate the virtual environment
 . venv/bin/activate
 
-# Start the application using Flask
-exec python -m flask run --host=0.0.0.0
+# Start the application using Waitress
+exec waitress-serve --port=5000 app:app
